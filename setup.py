c94d3a8e1ab9be62c9e6e37cb73f14914911ef60
mns.she <mns.she@gmail.com>
2024-07-10 08:01:31 +0530

    updated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup 
+
+setup(
+    name = "DiamondPricePrediction",
+    version= "0.0.1" ,
+    author= "M Shakila" ,
+    author_email= "ms@gmail.com" ,
+    #install_requires = get_requirements("requirements.txt") ,
+    packages=find_packages() 
+)
